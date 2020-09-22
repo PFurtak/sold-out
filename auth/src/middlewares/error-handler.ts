407529894be88718107ba642d620e427aa3fa1a1
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-22 00:29:26 -0400

    Created new subclass of error, and set a type check in error handler middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/db-conn-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,8 +8,15 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    console.log('request validation error');
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('db conn error');
+  }
+
   res.status(400).send({
-    message: 'Something went wrong',
+    message: err.message,
   });
 };
