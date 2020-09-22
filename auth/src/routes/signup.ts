05614da7a2346f20e7c51be2959fa9ef10266981
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-21 23:17:37 -0400

    Brought in express validator, added validation on signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('signed up');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Invalid email.'),
+    body('password')
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage('Password must be between 6 and 20 characters.'),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signUpRouter };
