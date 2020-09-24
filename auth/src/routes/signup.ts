11b1bd575d9516bbeb13eba228eacb36ca5823ef
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-23 22:34:45 -0400

    Wrote and tested user creation to the DB

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/db-conn-error';
 
 const router = express.Router();
 
@@ -23,9 +23,17 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating user');
-    throw new DatabaseConnectionError();
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
