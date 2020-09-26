f9a06d0204ba3afcf07500e0bdfe846703c94a2a
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-26 14:04:19 -0400

    Added user sign in and set jwt cookie

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { body } from 'express-validator';
 
 const router = express.Router();
@@ -11,7 +15,37 @@ router.post(
     body('password').trim().notEmpty().withMessage('Must provide a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signInRouter };
