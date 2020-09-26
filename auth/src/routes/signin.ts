dff40435de9cd2c024db27da8b385284f97ed38c
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-26 13:48:49 -0400

    Wrote validate request middleware to keep code DRY

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
@@ -10,13 +10,8 @@ router.post(
     body('email').isEmail().withMessage('Must provide a vaild email'),
     body('password').trim().notEmpty().withMessage('Must provide a password'),
   ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-  }
+  validateRequest,
+  (req: Request, res: Response) => {}
 );
 
 export { router as signInRouter };
