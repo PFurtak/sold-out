7db72d83ae1ecbec178ac5cdec24c1b41e899e12
Patrick Furtak <patrick@patrickfurtak.com>
2020-10-12 21:13:59 -0400

    Added user check on landing page, depending on if the browser or the server is performing the api request; we need to specify namespace and service for cross cluster communication within kubernetes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,26 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log('current user: ', currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'soldout.dev',
+        },
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
