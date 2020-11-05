6d283f841c88dd1cb533cd4da08d4b9554f72083
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-04 20:20:18 -0500

    Created test and setup route handler for /api/tickets

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,9 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
+    const response = await request(app).post('/api/tickets').send({});
+
+    expect(response.status).not.toEqual(404);
 
 })
 
