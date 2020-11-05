151c67084ac18a6f00576f5108971cac41c56e8b
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-04 21:15:58 -0500

    Added auth middleware to tickets post route, wrote tests for the tickets and a mock signed in function

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,7 +9,13 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 })
 
 it('can only be accessed if the user is signed in', async () => {
+    await request(app).post('/api/tickets').send({}).expect(401)
+})
+
+it('returns a status code other than 401 if user is authenticated', async () => {
+    const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({});
     
+    expect(response.status).not.toEqual(401);
 })
 
 it('returns an error if an invalid title is provided', async () => {
