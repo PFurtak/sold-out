d43c24d107d80534906dbc80064a6937ba627d2a
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-07 18:14:55 -0500

    Wrote handler for getting ticket by id, updated npm package on error-handler for better logging, updated test to use an official mongodb generated id

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,8 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@soldout-dev/common';
-import {createTicketRouter} from './routes/new';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show'
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,12 @@
-import request from 'supertest'
-import { app } from '../../app'
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 404 if ticket is not found', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+
     await request(app)
-    .get('/api/tickets/mcmksdmclskdmkf')
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
 })
