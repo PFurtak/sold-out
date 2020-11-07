95d6b8cbd44fcc2adb385da5938ae7eb4b41567a
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-07 18:34:59 -0500

    Wrote handler and tests for getting all tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,7 +4,8 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@soldout-dev/common';
 import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show'
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
