2319d319e7f6d301f2f3d732c04bc3bb4b51244b
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-09 16:51:18 -0500

    Accessed data from msg object on test listener

--- tickets/nats-test/src/listener.ts
+++ tickets/nats-test/src/listener.ts
@@ -1,16 +1,21 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
 
 const stan = nats.connect('ticketing', '123', {
     url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
-    console.log('Listener connected to NATS server')
+    console.log('Listener connected to NATS server');
 
-    const subscription = stan.subscribe('ticket:created')
+    const subscription = stan.subscribe('ticket:created');
 
-    subscription.on('message', (msg) => {
-        console.log('message received')
-    })
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
 
-})
+        if (typeof data === 'string') {
+            console.log(
+                `Received event #${msg.getSequence()}, with data: ${data}`
+            )
+        }
+    });
+});
