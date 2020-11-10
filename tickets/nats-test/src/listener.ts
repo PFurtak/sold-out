48bd23e6325f20d597738a13149db80e364541ee
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-09 22:07:33 -0500

    Randomly generated clientID on listener service to avoid name conflicts in NATS, subscribed listener to queue group

--- tickets/nats-test/src/listener.ts
+++ tickets/nats-test/src/listener.ts
@@ -1,13 +1,14 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
     console.log('Listener connected to NATS server');
 
-    const subscription = stan.subscribe('ticket:created');
+    const subscription = stan.subscribe('ticket:created', 'orderServiceQueueGroup');
 
     subscription.on('message', (msg: Message) => {
         const data = msg.getData();
