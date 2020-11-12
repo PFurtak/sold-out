b999464fe8f326a72a385aa536fde99db54c2106
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-12 17:01:34 -0500

    Created ticketcreated subclass of listener, verified functionality

--- tickets/nats-test/src/listener.ts
+++ tickets/nats-test/src/listener.ts
@@ -9,28 +9,11 @@ stan.on('connect', () => {
     console.log('Listener connected to NATS server');
 
     stan.on('close', () => {
-        console.log('NATS connection closed...')
+        console.log('NATS connection closed...');
         process.exit();
     })
 
-    const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('tickets-service');
-
-    const subscription = stan.subscribe('ticket:created', 'queue-group', options);
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-
-        if (typeof data === 'string') {
-            console.log(
-                `Received event #${msg.getSequence()}, with data: ${data}`
-            )
-        }
-        msg.ack();
-    });
+    new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -78,5 +61,16 @@ abstract class Listener {
         return typeof data === 'string'
         ? JSON.parse(data)
         : JSON.parse(data.toString('utf8'));
+    }
+}
+
+class TicketCreatedListener extends Listener {
+    subject = 'ticket:created';
+    queueGroupName = 'payments-service';
+
+    onMessage(data: any, msg: Message) {
+        console.log('Event data: ', data);
+
+        msg.ack();
     }
 }
