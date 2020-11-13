f1cb3eedeb14647315442162ba9c39234aad9e8e
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-13 15:02:48 -0500

    Configured publisher to return a promise when publishing an event

--- tickets/nats-test/src/publisher.ts
+++ tickets/nats-test/src/publisher.ts
@@ -6,23 +6,17 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS server');
     
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
-        id: '123',
-        title: 'concert',
-        price: 15,
-    });
-
-//     const data = JSON.stringify({
-//         id: '123',
-//         title: 'Concert',
-//         price: 20
-//     });
-
-//     stan.publish('ticket:created', data, () => {
-//         console.log('Event published')
-//     })
- })
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 15,
+        });  
+    } catch (err) {
+        console.error(err)
+    }
+ });
