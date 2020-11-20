e609ee855fcaa39573883a84bc86909f60cf0617
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-20 16:49:56 -0500

    Added nats-wrapper mock to test suites

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,11 +1,11 @@
-import request from 'supertest'
-import { app } from '../../app'
-import { Ticket } from '../../models/ticket'
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app).post('/api/tickets').send({});
 
-    expect(response.status).not.toEqual(404);
+    expect(response.status).not.toEqual(404)
 
 })
 
@@ -16,7 +16,7 @@ it('can only be accessed if the user is signed in', async () => {
 it('returns a status code other than 401 if user is authenticated', async () => {
     const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({});
 
-    expect(response.status).not.toEqual(401);
+    expect(response.status).not.toEqual(401)
 })
 
 it('returns an error if an invalid title is provided', async () => {
